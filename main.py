e22d17713e714bbba8b475ee9854bf6aa03b10de
4kamru <4kam@inbox.ru>
2024-08-04 09:33:00 +0500

    Add files via upload
    Базовые структуры данных, урок из вводной части 2024-08-03

---
+++ main.py
@@ -0,0 +1,15 @@
+# "1st program"
+print(9**0.5 * 5)
+
+# " 2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# " 3rd program"
+print(2*2 + 2)
+print(2*(2 + 2))
+print((2*2 + 2)==(2*(2 + 2)))
+
+# " 4th program"
+print(int((float('123.456')-int(float('123.456')))*10))
+# наверное, корректнее так, хотя Питон не ругается
+print(int((float('123.456')-float(int(float('123.456'))))*10))
