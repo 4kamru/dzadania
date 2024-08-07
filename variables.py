b0995bb1f3b073056a08e7a32ba9f3c972a33eb1
4kamru <4kam@inbox.ru>
2024-08-07 10:37:45 +0500

    Update variables.py
    Добавил в конце " часа" и имена переменных чуть поменял

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
-number_of_completed_dz = 12
-number_of_hours_spent = 1.5
+count_of_completed_dz = 12
+count_of_hours_spent = 1.5
 name_of_course = "Python"
-time_for_one_task = number_of_hours_spent / number_of_completed_dz
-print("Курс: ",name_of_course, "всего задач:",number_of_completed_dz,", затрачено часов: ",number_of_hours_spent,", среднее время выполнения ",time_for_one_task,".")
+time_for_one_task = count_of_hours_spent / count_of_completed_dz
+print("Курс: ",name_of_course, "всего задач:",count_of_completed_dz,", затрачено часов: ",count_of_hours_spent,", среднее время выполнения ",time_for_one_task," часа")
