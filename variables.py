9adc25a9f5ab6d25a04e43e2a25a47ca4a0a6df7
4kamru <4kam@inbox.ru>
2024-08-07 10:40:26 +0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,4 +2,4 @@ count_of_completed_dz = 12
 count_of_hours_spent = 1.5
 name_of_course = "Python"
 time_for_one_task = count_of_hours_spent / count_of_completed_dz
-print("Курс: ",name_of_course, "всего задач:",count_of_completed_dz,", затрачено часов: ",count_of_hours_spent,", среднее время выполнения ",time_for_one_task," часа")
+print("Курс: ",name_of_course, ", всего задач:",count_of_completed_dz,", затрачено часов: ",count_of_hours_spent,", среднее время выполнения ",time_for_one_task," часа")
