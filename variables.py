33a4af2eb6feaf42bab377af2e16d3985e604802
4kamru <4kam@inbox.ru>
2024-08-13 11:10:29 +0500

    Update variables.py
    Закомментировал вариант строки, который был выслан в ответ на замечание преподавателя 07-08-2024 в 08-38 и, видимо, не проверено.
    Новый вариант от 13-08-2024 - строка вывода приведена в соответствие с требованиями.

--- variables.py
+++ variables.py
@@ -2,4 +2,10 @@ count_of_completed_dz = 12
 count_of_hours_spent = 1.5
 name_of_course = "Python"
 time_for_one_task = count_of_hours_spent / count_of_completed_dz
-print("Курс: ",name_of_course, ", всего задач:",count_of_completed_dz,", затрачено часов: ",count_of_hours_spent,", среднее время выполнения ",time_for_one_task," часа")
+# старый вариант строки вывода
+# print("Курс: ",name_of_course, ", всего задач:",count_of_completed_dz,", затрачено часов: ",count_of_hours_spent,", среднее время выполнения ",time_for_one_task," часа")
+# вариант от 2024-08-13
+print(f"{"Курс:"} {name_of_course},{" всего задач:"}"
+      f"{count_of_completed_dz},{" затрачено часов:"}"
+      f" {count_of_hours_spent},{" среднее время выполнения "}"
+      f"{time_for_one_task} {" часа."}")
