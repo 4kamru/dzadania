e4051682b876ba6e33e4273dec01f40a9e89ecf7
4kamru <4kam@inbox.ru>
2024-08-17 14:50:02 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,30 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string=''):
+    t = {len(string), string.upper(), string.lower()}
+    count_calls()
+    return (t)
+
+
+def is_contains(string='', list_to_search=[]):
+    res = False
+    for i in range(len(list_to_search)):
+        if list_to_search[i] in string.lower():
+            res = True
+            break
+
+    count_calls()
+    return (res)
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
