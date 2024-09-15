5c1a6304a32812ccca5c949721e8f42ed85bf011
4kamru <4kam@inbox.ru>
2024-09-15 16:59:57 +0500

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -19,22 +19,28 @@ class House:
         return('Название: '+ self.name + ', кол-во этажей: ' + str(self.number_of_floors))
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other,House):
+            return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other,House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
         if isinstance(value, int):
@@ -43,13 +49,11 @@ class House:
 
     def __radd__(self, value):
         if isinstance(value, int):
-            self.number_of_floors += value
-            return(self)
+            return self + value
 
     def __iadd__(self, value):
         if isinstance(value, int):
-            self.number_of_floors += value
-            return(self)
+            return self + value
 
 
 
@@ -77,5 +81,3 @@ print(h1 >= h2) # __ge__
 print(h1 < h2) # __lt__
 print(h1 <= h2) # __le__
 print(h1 != h2) # __ne__
-
-
